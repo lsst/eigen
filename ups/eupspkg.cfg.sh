5c7e7a8f430e35e7b8ff1224d048105778145cc1
Jim Bosch <jbosch@astro.princeton.edu>
2017-09-13 19:31:35 -0400

    Use CMake build to install

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -1,18 +1,13 @@
 # EupsPkg config file. Sourced by 'eupspkg'
 
+build()
+{
+	(mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$PREFIX .. && make)
+}
+
 install()
 {
-	#
-	# Eigen uses cmake, but fortunately the scripts are easy enough to
-	# emulate w/o having to have cmake as a dependency
-	#
-
 	clean_old_install
-
-	mkdir -p "$PREFIX/include/unsupported"
-	cp -r Eigen "$PREFIX/include"
-	cp -r unsupported/Eigen "$PREFIX/include/unsupported"
-	ln -fs "$PREFIX/include/Eigen/Core" "$PREFIX/include/Eigen/Core.h"
-
+	(cd build && make install)
 	install_ups
 }
